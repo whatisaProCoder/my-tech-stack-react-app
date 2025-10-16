dfe3b8edc8ee6dd4f0e591ae683224a9ab1d9fe9
whatisaProCoder <debnathpritam2005@gmail.com>
2025-10-16 19:35:40 +0530

    update

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+
 function Technology(props) {
+  const [selected, setSelected] = useState(false);
+
+  const handleClick = () => {
+    setSelected(!selected);
+  };
+
   return (
-    <div className="border-1 border-[#4e57ca] text-[1rem] text-left px-4 py-2 rounded bg-[#333333] border-l-6 transition-all hover:translate-y-[-0.125rem] hover:drop-shadow-[2px_4px_8px_rgba(0,0,0,25%)]">
+    <div
+      onClick={handleClick}
+      style={{
+        borderColor: selected ? "#e1e1e1" : "#aeaeae",
+        fontWeight: selected ? "600" : "normal",
+        backgroundColor: selected ? "#454545" : "#333333",
+        borderLeftWidth: selected ? "6px" : "2px",
+      }}
+      className="border-2 text-[1rem] text-left px-4 py-2 rounded transition-all hover:translate-y-[-0.125rem] hover:drop-shadow-[2px_4px_8px_rgba(0,0,0,25%)] select-none"
+    >
       {props.name}
     </div>
   );
